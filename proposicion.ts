359d7275d88eab66ecbee57d56820ed96d2a64f0
FacundoRetamar <facundo1retamar@gmail.com>
2023-04-27 18:56:43 -0300

    2do commit

---
+++ proposicion.ts
@@ -0,0 +1,25 @@
+// proposicion la tipeamos como "p" y no prosicion "-p"
+let : p:"enero es un mes caluroso";
+let : p:"enero no es un mes caluroso";
+
+
+// en el and(y) se simboliza "p^q" (disyuncion)
+// para que sea una composicion compuesta verdadera tiene que ser las 2,3,4..etc verdaderas, si son
+// verdadera y falsa: es falsa : si es falsa y verdadera: es falsa, si es falsa y falsa: es falsa.
+
+// en el or(o) se simboliza p v q (conyuccion)
+// es falsa solo si o si "p y q son falsas", es verdadera en todos los demas casos.
+
+// en el entonces se simboliza ""p=>q""(implicacion)
+// esta es falsa solamente cuando p es verdadera y q es falsa.
+
+
+// tautologia(algo es totalmente verdad):"el cielo es azul"
+// contradiccion (algo que es totalmente lo contrario): "baje hacia arriba"
+
+
+// si solo si (p <=> q) "el nene sale a jugar solo si hizo la tarea"
+// p(v) <=> q(v) = v
+// p(f) <=> q(v) = f
+// p(v) <=> q(f) = f
+// p(f) <=> q(f) = v
